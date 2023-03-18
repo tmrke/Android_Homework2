2e7390b58948bbeda28d3ed076a5ccec583cdc14
tmrke <namat9@yandex.ru>
2023-03-18 22:35:44 +0700

    Initial commit

---
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.ageev.android_homework2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
