7d95fa22452e3cba2374e5b2ed22e2f466e0b124
tmrke <namat9@yandex.ru>
2023-03-24 20:17:29 +0700

    added cards

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -2,10 +2,15 @@ package ru.ageev.android_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.ageev.android_homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        binding = ActivityMainBinding.inflate(layoutInflater) // так можно достать view
         setContentView(R.layout.activity_main)
+//        setContentView(binding.root)
     }
 }
