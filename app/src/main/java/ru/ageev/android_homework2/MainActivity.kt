6fba70ce82303a56c72ef05bdc1b08aa04a34bce
tmrke <namat9@yandex.ru>
2023-03-25 12:08:41 +0700

    added menu and scroll

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -2,11 +2,10 @@ package ru.ageev.android_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.ageev.android_homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        binding = ActivityMainBinding.inflate(layoutInflater) // так можно достать view
