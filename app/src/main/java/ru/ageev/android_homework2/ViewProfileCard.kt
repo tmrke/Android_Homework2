db9fedb674ba819246de92e897f0e2b18d6706d8
tmrke <namat9@yandex.ru>
2023-03-28 18:06:54 +0700

    edit view profile card

---
+++ app/src/main/java/ru/ageev/android_homework2/ViewProfileCard.kt
@@ -0,0 +1,9 @@
+package ru.ageev.android_homework2;
+
+import android.content.Context
+import android.util.AttributeSet
+import kotlin.jvm.JvmOverloads;
+
+public class ViewProfileCard @JvmOverloads constructor(
+    context: Context, attrSet: AttributeSet? = null, defAttrsSet: Int = 0
+)
