88aff4b81bec0949b4a9dd8ca2048914cf57cfd8
tmrke <namat9@yandex.ru>
2023-03-28 20:12:46 +0700

    added custom view

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -2,14 +2,18 @@ package ru.ageev.android_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.ageev.android_homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        binding = ActivityMainBinding.inflate(layoutInflater) // так можно достать view
-        setContentView(R.layout.activity_main)
-//        setContentView(binding.root)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.ViewProfileCard.setUserNameText("Ololo")
+        binding.ViewProfileCard.changeColorButton()
     }
 }
