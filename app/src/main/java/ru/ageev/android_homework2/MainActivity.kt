8b072fd8bff61b59e5090c926440ce72989e09d8
Агеев Тимур <108726902+tmrke@users.noreply.github.com>
2023-03-28 20:14:54 +0700

    Revert "Develop"

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -2,18 +2,10 @@ package ru.ageev.android_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import ru.ageev.android_homework2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.ViewProfileCard.setUserNameText("Ololo")
-        binding.ViewProfileCard.changeColorButton()
+        setContentView(R.layout.activity_main)
     }
 }
