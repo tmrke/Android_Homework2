55a243617d537d864da8faee445727045a784cff
tmrke <namat9@yandex.ru>
2023-04-14 16:33:53 +0700

    add adapters

--- app/src/main/java/ru/ageev/android_homework2/images_screen/ImagesAdapter.kt
+++ app/src/main/java/ru/ageev/android_homework2/images_screen/ImagesAdapter.kt
@@ -1,22 +1,24 @@
 package ru.ageev.android_homework2.images_screen
+
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import ru.ageev.android_homework2.databinding.ActivityImagesBinding
+import ru.ageev.android_homework2.databinding.ItemImageBinding
 
-class ImagesAdapter : RecyclerView.Adapter<ImagesViewHolderScreen>() {
+class ImagesAdapter(private val images: List<ImageData>) :
+    RecyclerView.Adapter<ImagesViewHolderScreen>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagesViewHolderScreen {
         val binding =
-            ActivityImagesBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+            ItemImageBinding.inflate(LayoutInflater.from(parent.context), parent, false)
 
         return ImagesViewHolderScreen(binding)
     }
 
     override fun onBindViewHolder(holder: ImagesViewHolderScreen, position: Int) {
-        holder.bind()
+        holder.bind(images[position])
     }
 
     override fun getItemCount(): Int {
-        return 1
+        return images.size
     }
 }
