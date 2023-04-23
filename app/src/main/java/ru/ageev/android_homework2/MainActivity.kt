4ae5d34926829d4365f131fd8fc1bbe37a4976f8
tmrke <namat9@yandex.ru>
2023-04-23 12:21:17 +0700

    added delegates

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -3,7 +3,10 @@ package ru.ageev.android_homework2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.ConcatAdapter
+import by.kirich1409.viewbindingdelegate.CreateMethod
+import by.kirich1409.viewbindingdelegate.viewBinding
 import ru.ageev.android_homework2.databinding.ActivityMainBinding
+import ru.ageev.android_homework2.databinding.ActivityPostBinding
 import ru.ageev.android_homework2.first_screen.collage.CollageAdapter
 import ru.ageev.android_homework2.first_screen.collage.CollageData
 import ru.ageev.android_homework2.first_screen.post.PostAdapter
@@ -13,15 +16,13 @@ import ru.ageev.android_homework2.first_screen.profile.ProfileData
 import ru.ageev.android_homework2.images_screen.ImageData
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
-    //TODO поменять на делегат
+  private val binding: ActivityMainBinding by viewBinding(createMethod = CreateMethod.INFLATE)
 
     private val dataList = List(30) { ImageData() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val profileAdapter = ProfileAdapter(listOf(ProfileData()))
@@ -38,6 +39,6 @@ class MainActivity : AppCompatActivity() {
             startActivity(PostActivity.createIntent(this, PostData()))
         }
 
-        binding.recyclerView.adapter =  ConcatAdapter(profileAdapter, collageAdapter, postAdapter)
+        binding.recyclerView.adapter = ConcatAdapter(profileAdapter, collageAdapter, postAdapter)
     }
 }
