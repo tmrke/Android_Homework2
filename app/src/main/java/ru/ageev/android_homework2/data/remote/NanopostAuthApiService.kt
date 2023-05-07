fb27f0addeac4756de541adbf543943f840ea817
tmrke <namat9@yandex.ru>
2023-05-07 20:26:50 +0700

    added confirm password

--- app/src/main/java/ru/ageev/android_homework2/data/remote/NanopostAuthApiService.kt
+++ app/src/main/java/ru/ageev/android_homework2/data/remote/NanopostAuthApiService.kt
@@ -5,6 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.ageev.android_homework2.data.PrefsStorage
 import ru.ageev.android_homework2.data.remote.model.RegistrationRequest
 import ru.ageev.android_homework2.data.remote.model.response.CheckUsernameEnumResponse
 import ru.ageev.android_homework2.data.remote.model.response.CheckUsernameResponse
@@ -20,5 +21,5 @@ interface NanopostAuthApiService {
     @POST("api/auth/register")
     fun register(
         @Body registrationRequest: RegistrationRequest
-    )
+    ): PrefsStorage
 }
