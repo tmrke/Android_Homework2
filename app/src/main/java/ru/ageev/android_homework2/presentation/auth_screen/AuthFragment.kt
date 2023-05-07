402dc0f25b87a972ed5a378f62f39c8b5c17fa32
tmrke <namat9@yandex.ru>
2023-05-07 23:08:46 +0700

    added confirm password

--- app/src/main/java/ru/ageev/android_homework2/presentation/auth_screen/AuthFragment.kt
+++ app/src/main/java/ru/ageev/android_homework2/presentation/auth_screen/AuthFragment.kt
@@ -11,6 +11,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.navigation.Navigation
 import by.kirich1409.viewbindingdelegate.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
+import dagger.multibindings.IntKey
 import kotlinx.coroutines.launch
 import ru.ageev.android_homework2.R
 import ru.ageev.android_homework2.data.PrefsStorage
@@ -18,12 +19,12 @@ import ru.ageev.android_homework2.data.remote.model.RegistrationRequest
 import ru.ageev.android_homework2.data.remote.model.response.CheckUsernameEnumResponse
 import ru.ageev.android_homework2.databinding.FragmentAuthorizationBinding
 import ru.ageev.android_homework2.presentation.profile_screen.posts.PostsViewModel
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class AuthFragment : Fragment(R.layout.fragment_authorization) {
     private val binding by viewBinding(FragmentAuthorizationBinding::bind)
     private val viewModel by viewModels<AuthViewModel>()
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -35,20 +36,31 @@ class AuthFragment : Fragment(R.layout.fragment_authorization) {
                 val username = binding.editTextUsername.text.toString()
                 viewModel.checkUsername(username)
 
+
+                @Inject
                 if (textInputLayoutTextPassword.visibility == View.VISIBLE
                     && textInputLayoutTextPasswordConfirm.visibility == View.VISIBLE
                     && editTextPassword.text.toString().isNotEmpty()
                     && editTextPasswordConfirm.text.toString().isNotEmpty()
                 ) {
-                    val registrationRequest = RegistrationRequest(
-                        editTextUsername.text.toString(),
-                        editTextPasswordConfirm.text.toString()
-                    )
+                    if(editTextPassword.text.toString() == editTextPasswordConfirm.text.toString()){
+                        val registrationRequest = RegistrationRequest(
+                            editTextUsername.text.toString(),
+                            editTextPasswordConfirm.text.toString()
+                        )
 
-                    viewModel.register(registrationRequest)
+                        viewModel.register(registrationRequest)
+                        Toast.makeText(context, registrationRequest.username, Toast.LENGTH_SHORT).show()
+                    } else{
+                        Toast.makeText(context, "passwords must coincide", Toast.LENGTH_SHORT).show()
+                    }
                 } else if (textInputLayoutTextPassword.visibility == View.VISIBLE
                 ) {
-                    textInputLayoutTextPasswordConfirm.visibility = View.VISIBLE
+                    if(editTextPassword.text.toString().isNotEmpty()){
+                        textInputLayoutTextPasswordConfirm.visibility = View.VISIBLE
+                    } else{
+                        Toast.makeText(context, "password can't be empty", Toast.LENGTH_SHORT).show()
+                    }
                 }
             }
         }
