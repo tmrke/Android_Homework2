c8ed6bb1993c8b4ebbd9d73fc7a1429fadfcbad8
tmrke <namat9@yandex.ru>
2023-05-11 13:04:19 +0700

    added checkToken

--- app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
+++ app/src/main/java/ru/ageev/android_homework2/MainActivity.kt
@@ -2,21 +2,53 @@ package ru.ageev.android_homework2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
+import ru.ageev.android_homework2.data.PrefsStorage
 import ru.ageev.android_homework2.databinding.ActivityMainBinding
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var prefsStorage: PrefsStorage
     private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        //если токен сохранен то в логин
-        //возможно будет 2 навграфа
-        //сделать свою viewModel и обратиться к prefs для того что бы выбрать свой nav_graph
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val token = prefsStorage.token
+
+        val navGraph = navController.navInflater.inflate(
+            if (token != null) {
+                R.navigation.nav_graph
+            } else {
+                R.navigation.nav_graph_auth
+            }
+        )
+
+        navGraph.setStartDestination(
+            if (token != null) {
+                R.id.profileFragment
+            } else {
+                R.id.authFragment
+            }
+        )
+
+        navController.setGraph(navGraph, savedInstanceState)
     }
 }
--- app/src/main/java/ru/ageev/android_homework2/data/remote/NanopostAuthApiService.kt
+++ app/src/main/java/ru/ageev/android_homework2/data/remote/NanopostAuthApiService.kt
@@ -3,11 +3,8 @@ package ru.ageev.android_homework2.data.remote
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
-import retrofit2.http.Path
 import retrofit2.http.Query
-import ru.ageev.android_homework2.data.PrefsStorage
 import ru.ageev.android_homework2.data.remote.model.RegistrationRequest
-import ru.ageev.android_homework2.data.remote.model.response.CheckUsernameEnumResponse
 import ru.ageev.android_homework2.data.remote.model.response.CheckUsernameResponse
 import ru.ageev.android_homework2.data.remote.model.response.TokenResponse
 
